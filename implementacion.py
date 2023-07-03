648993ee71ea8741217e135540ec8b36b621603d
stevenML <58661882+zer0-sh@users.noreply.github.com>
2023-07-02 22:06:04 -0500

    CorreciónFinal

--- implementacion.py
+++ implementacion.py
@@ -1,13 +1,22 @@
 import simpy
 import random
+import numpy as np
+
+### GENERACION DEL TIEMPO DE SERVICIO ALEATORIAMENTE
+
+tiempo_consignacion = np.maximum(np.random.exponential(scale=4), 1)
+tiempo_consulta = np.maximum(np.random.exponential(scale=1), 1)
+tiempo_retiro = np.maximum(np.random.exponential(scale=3), 1)
 
 # Variables globales
+tiempo_max_espera_banco = 5  # Tiempo máximo de espera en el banco (minutos)
+
 tiempo_simulacion = 60  # Tiempo de simulación en minutos
 media_llegada_clientes = 1  # Media de llegada de clientes (clientes por minuto)
 media_tiempo_servicio = {
-    'consignacion': 4,  # Media de tiempo de servicio para consignaciones (minutos)
-    'consulta': 1,  # Media de tiempo de servicio para consultas (minutos)
-    'retiro': 3  # Media de tiempo de servicio para retiros (minutos)
+    'consignacion': tiempo_consignacion,  # Media de tiempo de servicio para consignaciones (con media de 4 minutos)
+    'consulta': tiempo_consulta,  # Media de tiempo de servicio para consultas (con media de 1 minutos)
+    'retiro': tiempo_retiro  # Media de tiempo de servicio para retiros (con media de 3 minutos)
 }
 probabilidad_tarjeta = 0.3  # Probabilidad de que un cliente realice una operación con tarjeta
 tiempo_atencion_tarjeta = 2  # Tiempo de atención promedio para clientes con tarjeta (minutos)
@@ -28,7 +37,8 @@ class Banco(object):
 
     def llegada_cliente(self):
         while True:
-            yield self.env.timeout(random.expovariate(media_llegada_clientes))  # Genera el tiempo entre llegadas de clientes
+            tiempo_llegada = np.random.poisson(media_llegada_clientes)  # Genera el tiempo entre llegadas de clientes con distribución de Poisson
+            yield self.env.timeout(tiempo_llegada)
             tipo_transaccion = self.get_tipo_transaccion()  # Obtiene el tipo de transacción aleatoriamente
             tiempo_servicio = self.get_tiempo_servicio(tipo_transaccion)  # Obtiene el tiempo de servicio para el tipo de transacción
             cliente = {
@@ -42,9 +52,13 @@ class Banco(object):
         llegada_cliente = self.env.now
         with self.cajeros.request() as request:
             yield request
+            espera_banco = self.env.now - llegada_cliente
+            if espera_banco > tiempo_max_espera_banco:
+                self.clientes_perdidos += 1  # Registra un cliente perdido si la espera en el banco excede el límite
+                return  # El cliente se va sin ser atendido
             espera = self.env.now - llegada_cliente
             if espera > tiempo_max_espera_tarjeta and cliente['con_tarjeta']:
-                self.clientes_perdidos += 1  # Registra un cliente perdido si la espera excede el límite para clientes con tarjeta
+                self.clientes_perdidos += 1  # Registra un cliente perdido si la espera en el cajero excede el límite para clientes con tarjeta
             else:
                 yield self.env.timeout(cliente['tiempo_servicio'])  # Simula el tiempo de atención al cliente
                 self.num_clientes_atendidos += 1  # Incrementa el contador de clientes atendidos
@@ -67,14 +81,14 @@ class Banco(object):
     def get_con_tarjeta(self):
         return random.random() < probabilidad_tarjeta  # Retorna True si el número aleatorio generado es menor a la probabilidad de tener tarjeta
 
-def imprimir_resultados(num_cajeros, num_clientes, porcentaje_utilizacion, porcentaje_perdidos, porcentaje_clientes_con_tarjeta, tiempo_atencion_total):
+def imprimir_resultados(num_cajeros, num_clientes, porcentaje_utilizacion, num_clientes_perdidos, num_clientes_con_tarjeta, tiempo_atencion_total):
     print("Resultados de la simulación:")
     print(f"Número de cajeros: {num_cajeros}")
     print(f"Número de clientes atendidos: {num_clientes}")
-    print(f"Porcentaje de utilización: {format(porcentaje_utilizacion, '.5f')}%")
-    print(f"Porcentaje de clientes perdidos: {format(porcentaje_perdidos, '.5f')}%")
-    print(f"Porcentaje de clientes con tarjeta: {format(porcentaje_clientes_con_tarjeta, '.5f')}%")
-    print(f"Tiempo de atención total: {format(tiempo_atencion_total, '.5f')} minutos")
+    print(f"Porcentaje de utilización del banco: {format(porcentaje_utilizacion, '.5f')}%")
+    print(f"Número de clientes perdidos: {num_clientes_perdidos}")
+    print(f"Número de clientes con tarjeta: {num_clientes_con_tarjeta}")
+    print(f"Tiempo de atención total: {format(tiempo_atencion_total, '.0f')} minutos")
 
     if porcentaje_utilizacion_inferior <= porcentaje_utilizacion <= porcentaje_utilizacion_superior:
         print("Cumple con la tasa de utilización")
@@ -85,15 +99,14 @@ def run_simulacion(num_cajeros):
     env = simpy.Environment()  # Crea el entorno de simulación
     banco = Banco(env, num_cajeros)  # Crea una instancia del banco
     env.process(banco.llegada_cliente())  # Inicia el proceso de llegada de clientes
-    env.run(until=tiempo_simulacion)  # Ejecuta la simulación hasta que se alcance el tiempo máximo
+    env.run(until=tiempo_simulacion)  # Ejecuta la simulación hasta el tiempo especificado
 
-    num_clientes = banco.num_clientes_atendidos + banco.clientes_perdidos  # Calcula el número total de clientes
-    porcentaje_utilizacion = (banco.tiempo_atencion_total / (tiempo_simulacion * num_cajeros)) * 100  # Calcula el porcentaje de utilización de los cajeros basado en tiempo total
-    porcentaje_perdidos = (banco.clientes_perdidos / num_clientes) * 100  # Calcula el porcentaje de clientes perdidos
-    porcentaje_clientes_con_tarjeta = (banco.num_clientes_con_tarjeta / num_clientes) * 100  # Calcula el porcentaje de clientes con tarjeta
+    # Cálculo del porcentaje de utilización del banco
+    porcentaje_utilizacion = (banco.tiempo_atencion_total / (num_cajeros * tiempo_simulacion)) * 100
 
-    imprimir_resultados(num_cajeros, banco.num_clientes_atendidos, porcentaje_utilizacion, porcentaje_perdidos, porcentaje_clientes_con_tarjeta, banco.tiempo_atencion_total)
+    # Imprimir resultados
+    imprimir_resultados(num_cajeros, banco.num_clientes_atendidos, porcentaje_utilizacion, banco.clientes_perdidos,
+                        banco.num_clientes_con_tarjeta, banco.tiempo_atencion_total)
 
-# ZONA DE PRUEBAS DE IMPLEMENTACION - Ejecutar simulación con diferentes números de cajeros
-#num_cajeros = 6
-#run_simulacion(num_cajeros)
+# Ejecutar simulación con 1 cajero
+run_simulacion(1)
